e0b66a9d509543f3293d62eba99d284d0a5af115
Kristian Virmo <kristian.virmo@digia.com>
2017-01-06 18:45:50 +0200

    CountryServiceWrapper implementation
    - Wraps ICountryService methods under proxy implementation
    - Missing async implementations

--- WcfProxy/CountryServiceWrapper.cs
+++ WcfProxy/CountryServiceWrapper.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using WcfProxy.CountryServiceReference;
 
 namespace WcfProxy
 {
-    public class CountryServiceWrapper
+    public class CountryServiceWrapper : ICountryService
     {
+        private string endpointUrl = "http://localhost:59546/ICountryService.svc";
+
+        public CountryServiceWrapper()
+        {
+        }
+        
+        public Country[] GetCountries()
+        {
+            var clientProxy = new Proxy.WcfProxy<ICountryService>(endpointUrl);
+            return clientProxy.Execute(client => client.GetCountries());
+        }
+
+        public Task<Country[]> GetCountriesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SaveCountry(Country country)
+        {
+            var clientProxy = new Proxy.WcfProxy<ICountryService>(endpointUrl);
+            clientProxy.Execute(client => client.SaveCountry(country));
+        }
+
+        public Task SaveCountryAsync(Country country)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            var clientProxy = new Proxy.WcfProxy<ICountryService>(endpointUrl);
+            clientProxy.Execute(client => client.Clear());
+        }
+
+        public Task ClearAsync()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
