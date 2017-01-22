973327fea28394590cf817754b758660ff900aca
Kristian Virmo <kristian.virmo@digia.com>
2017-01-22 15:39:42 +0200

    Interface for the person service wrapper

---
+++ WcfProxy/Interfaces/IPersonServiceWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WcfProxy.Models;
+
+namespace WcfProxy.Interfaces
+{
+    public interface IPersonServiceWrapper
+    {
+        IEnumerable<Person> GetPersons();
+        Task<IEnumerable<Person>> GetPersonsAsync();
+        void SavePerson(Person person);
+        Task SavePersonAsync(Person person);
+        void Clear();
+        Task ClearAsync();
+    }
+}
